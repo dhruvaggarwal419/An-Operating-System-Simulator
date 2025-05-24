e0ddfb5f8f3af42025108f738ff30a40603d5ba9
Dhruv Aggarwal <143499481+dhruvaggarwal419@users.noreply.github.com>
2025-05-24 15:51:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from disk_scheduling_python import disk_scheduling_app
 from memory_management_python import memory_management_app
 from deadlock_detection_python import deadlock_detection_app
 from page_fault_python import page_replacement_app
+from project_documentation import project_documentation_app
 
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Process Scheduling", "Disk Scheduling", "Memory Management (Visualization - Contiguous)", "Deadlock Detection", "Page Replacement Algorithms Simulator"])
+page = st.sidebar.radio("Go to", ["Process Scheduling", "Disk Scheduling", "Memory Management (Visualization - Contiguous)", "Deadlock Detection", "Page Replacement Algorithms Simulator", "Documentation"])
 if page == "Process Scheduling":
     process_scheduling_app()
 elif page == "Disk Scheduling":
@@ -17,3 +18,5 @@ elif page == "Deadlock Detection":
     deadlock_detection_app()
 elif page == "Page Replacement Algorithms Simulator":
     page_replacement_app()
+elif page == "Documentation":
+    project_documentation_app()
